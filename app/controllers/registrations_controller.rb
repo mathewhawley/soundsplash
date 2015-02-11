abccd4bf6ac0d02c42095ef269cfbb25938737c4
Mathew Hawley <mathew@mathewhawley.com>
2015-02-11 10:56:37 +0000

    Add ability to update user profile.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
   private
+
   def sign_up_params
     params.require(:user).permit(:profile_picture, :first_name, :last_name, :email, :about, :username, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:profile_picture, :first_name, :last_name, :email, :about, :username, :password, :password_confirmation, :current_password)
   end
 end
