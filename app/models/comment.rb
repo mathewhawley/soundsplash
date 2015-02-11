072a29fb3d2d294be2b224a786d3c1c92235ea5d
Mathew Hawley <mathew@mathewhawley.com>
2015-02-11 14:15:31 +0000

    Successfull censoring of profanity.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ActiveRecord::Base
+  require 'obscenity/active_model'
+
+  validates :content,  obscenity: { sanitize: true, replacement: :garbled }
+
   belongs_to :user
   belongs_to :track
+
 end
