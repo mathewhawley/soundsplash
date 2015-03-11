fd96d12b258496ea843b9f7ca8f7a854d52ca4b2
Mathew Hawley <mathew@mathewhawley.com>
2015-03-11 18:35:53 +0000

    Add active relationship association to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :tracks
+
+  has_many :active_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
